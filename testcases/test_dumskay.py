d1466c94df909b281bdb2cb7d1181d772f6db8d4
Green <name>
2023-01-28 10:24:40 +0200

    Task2_2

--- testcases/test_dumskay.py
+++ testcases/test_dumskay.py
@@ -1,7 +1,8 @@
-import pytest
 from example1 import register_user as positive_register
 from example_6 import register_user as empty_register
 from example_16 import register_user as password_diff
+from example9_1 import register_user_without_email
+from example13 import register_user_with_same_nick
 
 def test_user_registration_positive():
     a = positive_register()
@@ -13,4 +14,12 @@ def test_user_registration_all_empty():
 
 def test_user_registration_password_diff():
     a = password_diff()
+    assert a[0] == a[1]
+
+def test_email_empty():
+    a = register_user_without_email()
+    assert a[0] == a[1]
+
+def test_existing_user():
+    a = register_user_with_same_nick()
     assert a[0] == a[1]
